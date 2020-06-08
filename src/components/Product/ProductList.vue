41706b9f2bfb03cce27744d3605bd86494cfbf66
kadonomaro <denis.afer@gmail.com>
2020-06-08 22:30:03 +0700

    feat: add product-list component

---
+++ src/components/Product/ProductList.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="product-list">
+    product-list
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'product-list'
+}
+</script>
+
+<style lang="scss">
+  .product-list {
+
+  }
+</style>
---
+++ src/views/Category.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="category">
+    <product-list />
+  </div>
+</template>
+
+<script>
+
+import ProductList from '@/components/Product/ProductList.vue'
+
+export default {
+  name: 'category',
+  components: {
+    ProductList
+  }
+}
+</script>
