1b67bdd967913b00be9ae2d3dbdf27cf5eebe131
kadonomaro <denis.afer@gmail.com>
2020-07-11 19:59:56 +0700

    migrate: remove old files

--- src/filters/translate.filter.js
+++
@@ -1,6 +0,0 @@
-import { dictionary } from './translate.dictionary'
-
-export default function translateFilter (value) {
-  const result = dictionary[value] || value
-  return result.charAt(0).toUpperCase() + result.slice(1)
-}
--- src/server.settings.js
+++
@@ -1,3 +0,0 @@
-export const settings = {
-  url: 'http://localhost:1337'
-}
