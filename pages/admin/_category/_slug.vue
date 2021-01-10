2af551553f4f1af4d4fdf93b54dfd9ac2fbddcea
kadonomaro <denis.afer@gmail.com>
2021-01-10 17:45:21 +0700

    feat: update admin category page

--- pages/admin/_category/_slug.vue
+++ pages/admin/_category/_slug.vue
@@ -1,10 +1,66 @@
 <template>
-  <div class="container">
-    <button class="button button--accent" @click="$router.go(-1)">Назад</button>
-    <h1>{{ category.display_name }}</h1>
-    <div>ID: {{ category.id }}</div>
-    <div>Создан: {{ category.created_at | date }}</div>
-    <div>Изменен: {{ category.updated_at | date }}</div>
+  <div class="admin-category-page">
+    <div class="container">
+      <button class="button button--accent" @click="$router.go(-1)">
+        Назад
+      </button>
+      <h1>{{ category.display_name }}</h1>
+      <table class="table admin-table">
+        <tbody>
+          <tr>
+            <td>ID: </td>
+            <td>{{ category.id }}</td>
+          </tr>
+          <tr>
+            <td>Создан: </td>
+            <td>{{ category.created_at | date }}</td>
+          </tr>
+          <tr>
+            <td>Изменен: </td>
+            <td>{{ category.updated_at | date }}</td>
+          </tr>
+          <tr>
+            <td>Раздел активен: </td>
+            <td>
+              <label>
+                <input v-model="options.isActive" class="checkbox visually-hidden" type="checkbox">
+                <span class="checkbox-custom" />
+              </label>
+            </td>
+          </tr>
+          <tr>
+            <td>Показывать на главной: </td>
+            <td>
+              <label>
+                <input v-model="options.showOnMain" class="checkbox visually-hidden" type="checkbox">
+                <span class="checkbox-custom" />
+              </label>
+            </td>
+          </tr>
+          <tr>
+            <td>Сортировка: </td>
+            <td>
+              <label>
+                <input v-model="options.sort" class="input" type="number" min="0">
+              </label>
+            </td>
+          </tr>
+          <tr>
+            <td>Символьный код: </td>
+            <td>
+              <label>
+                <input v-model="options.slug" class="input" type="text">
+              </label>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <div class="controls">
+        <button class="button button--accent" title="Сохранить" @click="submitHandler">
+          Сохранить
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -19,7 +75,24 @@ export default {
   },
   data () {
     return {
-      category: []
+      category: {},
+      options: {
+        isActive: null,
+        sort: null,
+        slug: null,
+        showOnMain: null
+      }
+    }
+  },
+  mounted () {
+    this.options.isActive = this.category.active
+    this.options.sort = this.category.sort
+    this.options.slug = this.category.slug
+    this.options.showOnMain = this.category.show_on_main
+  },
+  methods: {
+    submitHandler () {
+      console.log(this.options)
     }
   }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -9,12 +9,13 @@ import { categoriesApi } from '@/api/categories.api'
 import CatList from '@/components/Category/CatList.vue'
 
 export default {
+  name: 'IndexPage',
   components: {
     CatList
   },
   async asyncData ({ $axios }) {
     const response = await categoriesApi($axios).getAll()
-    const categories = response.splice(0, 4)
+    const categories = response.filter(category => category.show_on_main)
     return { categories }
   },
   data () {
